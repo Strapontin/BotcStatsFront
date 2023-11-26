d77c57b58c59c5853356e77b786d88870b62b4d4
Strapontin <antodu73@hotmail.fr>
2023-11-26 17:39:30 +0100

    try fix env variables

--- botc-stats-front/next.config.js
+++ botc-stats-front/next.config.js
@@ -3,5 +3,11 @@ module.exports = {
   reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
+    DISCORD_ID: process.env.DISCORD_ID,
+    DISCORD_SECRET: process.env.DISCORD_SECRET,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NODE_TLS_REJECT_UNAUTHORIZED: process.env.NODE_TLS_REJECT_UNAUTHORIZED,
   },
 };
