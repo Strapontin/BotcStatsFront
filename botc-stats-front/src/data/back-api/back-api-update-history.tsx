2dec86830f744f2605e1fb2aa0ea2123d576b685
Strapontin <antodu73@hotmail.fr>
2024-01-17 14:02:05 +0100

    history filter id

--- botc-stats-front/src/data/back-api/back-api-update-history.tsx
+++ botc-stats-front/src/data/back-api/back-api-update-history.tsx
@@ -1,10 +1,23 @@
 import useSWR from "swr";
 import useApi from "./useApi";
 
-const fetcher = (url: string) => fetch(url).then((d) => d.json());
-
 export function useGetUpdateHistory() {
-  const { apiUrl, isLoadingApi } = useApi();
+  const { apiUrl, isLoadingApi, accessToken } = useApi();
+
+  const fetcher = (url: string) =>
+    fetch(url, {
+      method: "GET",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+    }).then((d) => d.json());
+
   const { data, error, isLoading } = useSWR(
     !isLoadingApi ? `${apiUrl}/UpdateHistory` : null,
     fetcher
--- botc-stats-front/src/pages/history/index.tsx
+++ botc-stats-front/src/pages/history/index.tsx
@@ -1,3 +1,4 @@
+import Filter from "@/components/filter/Filter";
 import {
   GenericTable,
   GenericTableColumnProps,
@@ -6,11 +7,12 @@ import {
 import { useGetUpdateHistory } from "@/data/back-api/back-api-update-history";
 import { UpdateHistory } from "@/entities/UpdateHistory";
 import { dateTimeToString } from "@/helper/date";
-import { Spinner, Tab, Tabs } from "@nextui-org/react";
+import { Spacer, Spinner, Tab, Tabs } from "@nextui-org/react";
+import { useMemo, useState } from "react";
 import NotFoundPage from "../404";
 
 type RowType = GenericTableRowsExtendedProps & {
-  id: number;
+  key: number;
   date: string;
   text: string;
 
@@ -22,6 +24,17 @@ type RowType = GenericTableRowsExtendedProps & {
 
 export default function HistoryPage() {
   const { data: updateHistory, isLoading } = useGetUpdateHistory();
+  const [filter, setFilter] = useState<string>("");
+
+  const filterId = useMemo(() => {
+    return (
+      <Filter
+        filterValue={filter}
+        placeholder="Filtrer sur l'id"
+        setFilter={setFilter}
+      />
+    );
+  }, [filter]);
 
   if (isLoading) {
     return <Spinner />;
@@ -31,7 +44,7 @@ export default function HistoryPage() {
 
   const genericTableColumns: GenericTableColumnProps[] = [
     {
-      key: "id",
+      key: "key",
       name: "Id",
       allowSorting: true,
       isDefaultVisible: true,
@@ -42,7 +55,6 @@ export default function HistoryPage() {
       allowSorting: true,
       isDefaultVisible: true,
       isDefaultSort: true,
-      reverseDefaultSort: true,
     },
     {
       key: "text",
@@ -54,7 +66,8 @@ export default function HistoryPage() {
 
   const tableRows = updateHistory.map((uh: UpdateHistory) => {
     const result: RowType = {
-      id: uh.id,
+      id: "uh" + uh.id,
+      key: uh.id,
 
       date: dateTimeToString(uh.date),
       text: uh.text,
@@ -66,9 +79,9 @@ export default function HistoryPage() {
 
       renderJSX: {
         text: (
-          <div className="flex text-left whitespace-break-spaces">
+          <span className="flex text-left whitespace-break-spaces">
             {uh.text}
-          </div>
+          </span>
         ),
       },
     };
@@ -78,45 +91,69 @@ export default function HistoryPage() {
   return (
     <Tabs aria-label="tabs">
       <Tab key="players" title="Joueurs">
+        {filterId}
+        <Spacer y={2.5} />
         <GenericTable
           columns={genericTableColumns}
           rows={tableRows
-            .filter((tr: RowType) => tr.playerId)
+            .filter(
+              (tr: RowType) =>
+                (tr.playerId && filter === "") ||
+                (!isNaN(Number(filter)) && Number(filter) === tr.playerId)
+            )
             .map((tr: RowType) => {
-              tr.id = tr.playerId!;
+              tr.key = tr.playerId!;
               return tr;
             })}
         />
       </Tab>
       <Tab key="roles" title="Rôles">
+        {filterId}
+        <Spacer y={2.5} />
         <GenericTable
           columns={genericTableColumns}
           rows={tableRows
-            .filter((tr: RowType) => tr.roleId)
+            .filter(
+              (tr: RowType) =>
+                (tr.roleId && filter === "") ||
+                (!isNaN(Number(filter)) && Number(filter) === tr.roleId)
+            )
             .map((tr: RowType) => {
-              tr.id = tr.roleId!;
+              tr.key = tr.roleId!;
               return tr;
             })}
         />
       </Tab>
       <Tab key="editions" title="Modules">
+        {filterId}
+        <Spacer y={2.5} />
         <GenericTable
           columns={genericTableColumns}
           rows={tableRows
-            .filter((tr: RowType) => tr.editionId)
+            .filter(
+              (tr: RowType) =>
+                (tr.editionId && filter === "") ||
+                (!isNaN(Number(filter)) && Number(filter) === tr.editionId)
+            )
             .map((tr: RowType) => {
-              tr.id = tr.editionId!;
+              tr.key = tr.editionId!;
               return tr;
             })}
         />
       </Tab>
       <Tab key="games" title="Parties">
+        {filterId}
+        <Spacer y={2.5} />
         <GenericTable
           columns={genericTableColumns}
           rows={tableRows
-            .filter((tr: RowType) => tr.gameId)
+            .filter(
+              (tr: RowType) =>
+                (tr.gameId && filter === "") ||
+                (!isNaN(Number(filter)) && Number(filter) === tr.gameId)
+            )
             .map((tr: RowType) => {
-              tr.id = tr.gameId!;
+              tr.key = tr.gameId!;
               return tr;
             })}
         />
