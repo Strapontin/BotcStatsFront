33b849c342b514b85f52b48d99eb3ca57ef8ea41
Strapontin <antodu73@hotmail.fr>
2024-01-29 23:54:26 +0100

    role icon hiding behind winning alignment

--- botc-stats-front/src/components/ui/image-role-name.tsx
+++ botc-stats-front/src/components/ui/image-role-name.tsx
@@ -26,7 +26,7 @@ export function RoleImageName(props: {
   );
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center flex-auto shrink-1 text-end ml-1">
       {props.name}
       {image}
     </div>
